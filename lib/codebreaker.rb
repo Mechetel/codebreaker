a5f7790f60f028625a20497492ca0ab19d4d89fa
mechetel <dima.homa5@gmail.com>
2021-03-05 16:25:52 +0200

    fix

--- lib/codebreaker.rb
+++ lib/codebreaker.rb
@@ -6,8 +6,6 @@ require_relative 'codebreaker/errors/name_is_not_string_error'
 require_relative 'codebreaker/errors/short_name_error'
 require_relative 'codebreaker/errors/long_name_error'
 require_relative 'codebreaker/errors/difficulty_error'
-require_relative 'codebreaker/errors/digit_range_error'
-require_relative 'codebreaker/errors/digits_count_error'
 require_relative 'codebreaker/errors/expected_difficulty_instance_error'
 require_relative 'codebreaker/errors/expected_user_instance_error'
 
