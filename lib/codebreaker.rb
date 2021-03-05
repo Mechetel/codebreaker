8aec67fa2b0f4643c4e97aa614e3ba7ba4d9d17d
mechetel <dima.homa5@gmail.com>
2021-03-05 15:54:51 +0200

    add libs to require

--- lib/codebreaker.rb
+++ lib/codebreaker.rb
@@ -1,4 +1,21 @@
 require_relative 'codebreaker/version'
 
-module Codebreaker
-end
+require 'yaml'
+
+require_relative 'codebreaker/errors/name_is_not_string_error'
+require_relative 'codebreaker/errors/short_name_error'
+require_relative 'codebreaker/errors/long_name_error'
+require_relative 'codebreaker/errors/difficulty_error'
+require_relative 'codebreaker/errors/digit_range_error'
+require_relative 'codebreaker/errors/digits_count_error'
+require_relative 'codebreaker/errors/expected_difficulty_instance_error'
+require_relative 'codebreaker/errors/expected_user_instance_error'
+
+require_relative 'codebreaker/modules/validator'
+
+require_relative 'codebreaker/services/statistics_service'
+
+require_relative 'codebreaker/entities/difficulty'
+require_relative 'codebreaker/entities/game'
+require_relative 'codebreaker/entities/guess_checker'
+require_relative 'codebreaker/entities/user'
