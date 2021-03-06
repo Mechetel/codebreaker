7e9cab49c9f9f6e5f263695ec40e7fd03308910c
mechetel <dima.homa5@gmail.com>
2021-03-06 10:04:54 +0200

    add errors to difficulty

--- lib/codebreaker/entities/difficulty.rb
+++ lib/codebreaker/entities/difficulty.rb
@@ -5,10 +5,11 @@ module Codebreaker
     DIFFICULTIES = { easy: { attempts: 15, hints: 2 },
                      medium: { attempts: 10, hints: 1 },
                      hell: { attempts: 5, hints: 1 } }.freeze
-    attr_reader :level
+    attr_reader :level, :errors
 
     def initialize(level)
       @level = level.to_sym
+      @errors = []
     end
 
     def attempts
