c17ba79f8fb3cbbd9b90433b852759e3977b7812
mechetel <dima.homa5@gmail.com>
2021-03-11 00:06:58 +0200

    remove unnecessary errors

--- lib/codebreaker/errors/expected_difficulty_instance_error.rb
+++
@@ -1,7 +0,0 @@
-module Codebreaker
-  class ExpectedDifficultyInstanceError < StandardError
-    def message
-      'Object should be an instance of Difficulty class'
-    end
-  end
-end
--- lib/codebreaker/errors/expected_user_instance_error.rb
+++
@@ -1,7 +0,0 @@
-module Codebreaker
-  class ExpectedUserInstanceError < StandardError
-    def message
-      'Object should be an instance of User class'
-    end
-  end
-end
