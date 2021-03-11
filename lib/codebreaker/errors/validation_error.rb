50e022eafddeaea92574f7953ed295475ccc13af
mechetel <dima.homa5@gmail.com>
2021-03-11 20:13:37 +0200

    undo last commit

--- lib/codebreaker/errors/validation_error.rb
+++ lib/codebreaker/errors/validation_error.rb
@@ -1,9 +1,7 @@
 module Codebreaker
   class ValidationError < StandardError
-    attr_reader :message
     def initialize(msg = 'Validation error')
       super(msg)
-      @message = msg
     end
   end
 end
