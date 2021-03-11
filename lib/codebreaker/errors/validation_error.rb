544b4969502f97a525dc3ae656ec47d993dad49b
mechetel <dima.homa5@gmail.com>
2021-03-11 20:07:25 +0200

    change logic of validation class

--- lib/codebreaker/errors/validation_error.rb
+++ lib/codebreaker/errors/validation_error.rb
@@ -1,7 +1,9 @@
 module Codebreaker
   class ValidationError < StandardError
+    attr_reader :message
     def initialize(msg = 'Validation error')
       super(msg)
+      @message = msg
     end
   end
 end
